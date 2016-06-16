9261985c2912c88ea2c2dac999d36b3da12542fa
ralphsol <harshil.7535@gmail.com>
2016-06-16 09:44:15 +0530

    nxcorr2 implementation

--- scroll.m
+++ scroll.m
@@ -5,20 +5,15 @@ sc_fin = double(rgb2gray(sc_fin));
 
 %won't hold if overlaps
 %imagesc(sc_in);
+%hold on
 %imagesc(sc_fin);
 
-cc = xcorr2(sc_fin,sc_in);
-[max_cc, imax] = max(abs(cc(:)));
-[ypeak, xpeak] = ind2sub(size(cc),imax(1));
+c = normxcorr2(sc_in,sc_fin);
+figure, surf(c), shading flat;
+[ypeak, xpeak] = find(c==max(c(:)));
+yoffSet = ypeak-size(sc_in,1);
+xoffSet = xpeak-size(sc_in,2);
 
-figure
-plot(cc(:))
-title('Cross-Correlation')
-hold on
-plot(imax,max_cc,'or')
-hold off
-text(imax*1.05,max_cc,'Maximum')
-
-corr_offset = [(ypeak-size(sc_in,1)) (xpeak-size(sc_in,2))];
+corr_offset = [(yoffSet) (xoffSet)];
 
 disp(corr_offset);
