773ff5f87e54340f74e37cd8eb69bdb4a8daef3c
ralphsol <harshil.7535@gmail.com>
2016-06-19 22:56:19 +0530

    Updated stroke diff

--- stroke_diff.m
+++ stroke_diff.m
@@ -1,6 +1,8 @@
 stroke_set = {[0 0; -1 0; 0 -1; -1 -1], [1 1;], [9 9; 7 7]};
 %stroke_set = {};
+%stroke_set = {[2 2;]};
 B = [2 2; 1 2; 1 1; 2 1];
+%B = [1 1; 2 1];
 
 while size(B, 1) > 0
    if size(stroke_set, 2) == 0
@@ -9,16 +11,20 @@ while size(B, 1) > 0
        continue;
    end
     
-   min_dist = 10000;
+   min_dist = inf;
    for k=1:size(stroke_set, 2)
        [M, I, J] = closeness(stroke_set{k}, B);
        %disp([M I J]);
        if M < min_dist
            min_dist = M;
            closest = k;
-           min_index = J;
+           min_index = J; %J final
        end
    end
+   
+   %disp(min_index);
+   %disp(B);
+   %disp(stroke_set{1});
    
    if min_dist > 10
        stroke_set{end+1} = B(1, :);
@@ -27,7 +33,6 @@ while size(B, 1) > 0
        stroke_set{closest} = [stroke_set{closest}; B(min_index, :)];
        B(min_index, :) = [];
    end
-   %disp(B);
 end
 
 for k=1:size(stroke_set, 2)
